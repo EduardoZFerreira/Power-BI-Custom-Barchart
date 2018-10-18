bb70494bebd167cfdcabcc4fac31cf27e2aced91
Eduardo Ferreira <eduardo.zekl@gmail.com>
2018-10-18 13:44:04 -0300

    Added code for initial square

--- src/visual.ts
+++ src/visual.ts
@@ -27,46 +27,30 @@
 module powerbi.extensibility.visual {
     "use strict";
     export class Visual implements IVisual {
-        private target: HTMLElement;
-        private updateCount: number;
-        private settings: VisualSettings;
-        private textNode: Text;
+        private svg: d3.Selection<SVGElement>;
 
         constructor(options: VisualConstructorOptions) {
-            console.log('Visual constructor', options);
-            this.target = options.element;
-            this.updateCount = 50;
-            if (typeof document !== "undefined") {
-                const new_p: HTMLElement = document.createElement("p");
-                new_p.appendChild(document.createTextNode("Update count:"));
-                const new_em: HTMLElement = document.createElement("em");
-                this.textNode = document.createTextNode(this.updateCount.toString());
-                new_em.appendChild(this.textNode);
-                new_p.appendChild(new_em);
-                this.target.appendChild(new_p);
-            }
+            this.svg = d3.select(options.element)
+            .append('svg')
+            .classed('barchart', true);
         }
 
         public update(options: VisualUpdateOptions) {
-            this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-            console.log('Visual update', options);
-            
-            if (typeof this.textNode !== "undefined") {
-                this.textNode.textContent = (this.updateCount++).toString();
-            }            
+             let width = options.viewport.width;
+             let height = options.viewport.height;
+
+             this.svg.attr({
+                width: width,
+                height: height
+             });
+             let rect = this.svg.append('rect')
+             .attr(
+             {
+                width: 50,
+                height: 50,
+                fill: 'red'
+             });    
         }
 
-        private static parseSettings(dataView: DataView): VisualSettings {
-            return VisualSettings.parse(dataView) as VisualSettings;
-        }
-
-        /** 
-         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
-         * objects and properties you want to expose to the users in the property pane.
-         * 
-         */
-        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-            return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-        }
     }
 }
